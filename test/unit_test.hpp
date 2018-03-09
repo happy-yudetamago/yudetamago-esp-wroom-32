712a6db86f84953691c023034ab6d0775f6e89fc
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2018-03-10 02:40:46 +0900

    Add UT_ASSERT_XXX

--- test/unit_test.hpp
+++ test/unit_test.hpp
@@ -2,9 +2,26 @@
 #pragma once
 
 #include <stdio.h>
+#include <assert.h>
 
 #define UT_RUN(func) \
     printf("----- %s:%d %s() start -----\n", __FILE__, __LINE__, #func); \
     func(); \
     printf("----- %s:%d %s() end   -----\n", __FILE__, __LINE__, #func); \
 
+#define UT_ASSERT_STRING(expected, actual)                              \
+    if (strcmp(expected, actual) != 0) {                                \
+        printf("assert fail\n  expected : %s\n  actual: %s\n",          \
+               expected, actual);                                       \
+        assert(false);                                                  \
+    }                                                                   \
+
+#define UT_ASSERT_POINTER(expected, actual)                             \
+    if (expected != actual) {                                           \
+        printf("assert fail\n  expected : %p\n  actual: %p\n",          \
+               expected, actual);                                       \
+        assert(false);                                                  \
+    }                                                                   \
+
+#define UT_ASSERT_TRUE(actual)  assert(actual)
+#define UT_ASSERT_FALSE(actual) assert(!actual)
